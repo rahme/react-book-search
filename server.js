af2ed4630d10cd7ee51b1a9fc45a400571387dd8
Raffay Ahmed <raf.amed97@gmail.com>
2020-10-10 20:59:16 -0700

    heroku

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-// Define API routes here
 app.use(routes)
-
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://raffayahmed:password123456789@cluster0.7vqtw.mongodb.net/test?retryWrites=true&w=majority" || process.env.MONGODB_URI, { useNewUrlParser: true });
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`Server open on ${PORT}`);
 });
